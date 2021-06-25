a8ee4ea3e189c015044c358978cb4d295661d077
shi514652697 <ssnegi_4all@yahoo.com>
2021-06-25 11:04:42 -0500

    first commit

---
+++ src/main/java/com/citi/UtilityFunctionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.citi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityFunctionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityFunctionProjectApplication.class, args);
+	}
+
+}
